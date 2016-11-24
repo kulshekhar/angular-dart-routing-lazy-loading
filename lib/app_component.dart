eaee444c62b2de87f12fdaca10ac1cdc57137e41
kulshekhar <kulshekhar@users.noreply.github.com>
2016-11-24 13:41:26 +0530

    Use AsyncRoute

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -24,19 +24,12 @@ import 'package:lazy_loading_ng2/mycomponent2.dart' deferred as c2;
     name: 'C1',
     component: C1Component,
   ),
+  const AsyncRoute(
+    path: '/c2',
+    name: 'C2',
+    loader: f,
+  ),
 ])
-class AppComponent {
-  final Router _r;
+class AppComponent {}
 
-  bool lazyLoaded = false;
-
-  AppComponent(this._r);
-
-  /// To automatically load a component place this code in `ngOnInit`. That will
-  /// decrease the initial payload and response time.
-  loadC2() async {
-    await c2.loadLibrary();
-    _r.config([new Route(path: '/c2', name: 'C2', component: c2.C2Component)]);
-    lazyLoaded = true;
-  }
-}
+f() => c2.loadLibrary().then((_) => c2.C2Component);
